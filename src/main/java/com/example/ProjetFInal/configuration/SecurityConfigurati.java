d3193d044a4a8c8818fddeb969e5c9aefd14b0f2
nyoumi <nyoumipaul@yahoo.fr>
2021-09-07 08:29:33 +0100

    added test link test auth

--- src/main/java/com/example/ProjetFInal/configuration/SecurityConfigurati.java
+++ src/main/java/com/example/ProjetFInal/configuration/SecurityConfigurati.java
@@ -45,7 +45,7 @@ public class SecurityConfigurati  extends WebSecurityConfigurerAdapter {
         http.csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/authenticate","/all","/createUser","/swagger-ui.html","/test")
+                .antMatchers("/auth/authenticate","/all","/createUser","/swagger-ui.html","/test")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
--- src/main/java/com/example/ProjetFInal/controllers/HelloController.java
+++ src/main/java/com/example/ProjetFInal/controllers/HelloController.java
@@ -1,20 +1,37 @@
 package com.example.ProjetFInal.controllers;
 
 
+import com.example.ProjetFInal.modeles.JwtRequest;
+import com.example.ProjetFInal.modeles.JwtResponse;
 import com.example.ProjetFInal.modeles.Utilisateur;
 import com.example.ProjetFInal.repositories.UtilisateurRepository;
+import com.example.ProjetFInal.services.UserService;
+import com.example.ProjetFInal.utility.JWTUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class HelloController {
-
+    private final JWTUtility jwtUtility;
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
     @Autowired
     public UtilisateurRepository utilisateurRepository;
+
+    public HelloController(JWTUtility jwtUtility, AuthenticationManager authenticationManager, UserService userService) {
+
+            this.jwtUtility = jwtUtility;
+            this.authenticationManager = authenticationManager;
+            this.userService = userService;
+    }
 
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping ("/all")
@@ -31,10 +48,24 @@ public class HelloController {
 
     @CrossOrigin(origins = "*")
     @GetMapping("/test")
-    public ResponseEntity<?> test(){
-        String response = "test ok";
+    public  JwtResponse authenticate(@RequestBody JwtRequest jwtRequest) throws Exception{
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            jwtRequest.getUsername(),
+                            jwtRequest.getPassword()
+                    )
+            );
+        }catch (BadCredentialsException e){
+            throw  new Exception("INVALID_CREDENTIAL",e);
+        }
+        final UserDetails userDetails
+                = userService.loadUserByUsername(jwtRequest.getUsername());
+        final String token =
+                jwtUtility.generateToken(userDetails);
+        Utilisateur logU = userService.findUser(jwtRequest.getUsername());
+        return new JwtResponse(token,logU);
 
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
 
