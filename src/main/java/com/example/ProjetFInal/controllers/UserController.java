b1a2b3d2b1bc49013a754fed2306f31d115b665b
nyoumi <nyoumipaul@yahoo.fr>
2021-09-15 18:57:43 +0100

    research

--- src/main/java/com/example/ProjetFInal/controllers/UserController.java
+++ src/main/java/com/example/ProjetFInal/controllers/UserController.java
@@ -50,7 +50,7 @@ public class UserController {
         for (Utilisateur user :users) {
             if( user.getId_pharma() != null && user.getId_pharma().trim().length() > 0 ){
                 System.out.println(user.getId_pharma());
-                //pharmacies.add(this.pharmacieService.getPharma(user.getId_pharma()).get(0)) ;
+                pharmacies.add(this.pharmacieService.getPharma(user.getId_pharma()).get(0)) ;
             }
 
         }
