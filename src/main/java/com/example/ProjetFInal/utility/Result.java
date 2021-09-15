86f38b6d2464ecc22471f5d0704c8cbc6cf65258
nyoumi <nyoumipaul@yahoo.fr>
2021-09-15 13:42:10 +0100

    sec

--- src/main/java/com/example/ProjetFInal/utility/Result.java
+++ src/main/java/com/example/ProjetFInal/utility/Result.java
@@ -1,12 +1,39 @@
 package com.example.ProjetFInal.utility;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
 public class Result {
     private String message;
     private int code;
 
+    public Result(String message, int code) {
+        this.message = message;
+        this.code = code;
+    }
+
+    public Result() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "message='" + message + '\'' +
+                ", code=" + code +
+                '}';
+    }
 }
