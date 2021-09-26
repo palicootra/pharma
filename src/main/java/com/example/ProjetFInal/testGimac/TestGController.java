8c7c2404c9b96a2c893d74171d3729e980e30962
paul nyoumi <nyoumipaul@yahoo.fr>
2021-09-26 07:11:32 +0100

    gimac

--- src/main/java/com/example/ProjetFInal/testGimac/TestGController.java
+++ src/main/java/com/example/ProjetFInal/testGimac/TestGController.java
@@ -15,7 +15,7 @@ public class TestGController {
 
     @CrossOrigin(origins = "*")
     @PostMapping("/oauth/token")
-    private Token create(@RequestBody Object input){
+    private Token create(@RequestBody Token input){
 
 
         return new Token("42c93229-59b2-49fd-9366-69f5e7177712",
