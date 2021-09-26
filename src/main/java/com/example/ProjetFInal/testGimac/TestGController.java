c6df79bb9b25b3871b140fb6ce01a75588c05038
paul nyoumi <nyoumipaul@yahoo.fr>
2021-09-26 05:57:14 +0100

    gimac

---
+++ src/main/java/com/example/ProjetFInal/testGimac/TestGController.java
@@ -0,0 +1,31 @@
+package com.example.ProjetFInal.testGimac;
+
+
+import com.example.ProjetFInal.modeles.Categories;
+import com.example.ProjetFInal.modeles.Medicament;
+import com.example.ProjetFInal.services.CatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+@RestController
+@RequestMapping("/pp")
+public class TestGController {
+
+
+
+    @CrossOrigin(origins = "*")
+    @PostMapping("/oauth/token")
+    private Token create(@RequestBody Object input){
+
+
+        return new Token("42c93229-59b2-49fd-9366-69f5e7177712",
+                "bearer","61ecd5fc-53bb-447e-bcb4-8d8873e59876",14400,"read");
+    }
+}
+
---
+++ src/main/java/com/example/ProjetFInal/testGimac/Token.java
@@ -0,0 +1,20 @@
+package com.example.ProjetFInal.testGimac;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Token {
+    String access_token;
+    String token_type;
+    String refresh_token;
+    long expires_in;
+    String scope;
+
+
+}
