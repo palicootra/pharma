450f285cce6a36d9be4992d8ad8368ae8a776902
paul nyoumi <nyoumipaul@yahoo.fr>
2021-09-26 10:55:00 +0100

    gimac expiresin

--- src/main/java/com/example/ProjetFInal/testGimac/TestGController.java
+++ src/main/java/com/example/ProjetFInal/testGimac/TestGController.java
@@ -20,7 +20,7 @@ public class TestGController {
 
 
         return new Token("42c93229-59b2-49fd-9366-69f5e7177712",
-                "bearer","61ecd5fc-53bb-447e-bcb4-8d8873e59876",360,"read");
+                "bearer","61ecd5fc-53bb-447e-bcb4-8d8873e59876",300,"read");
     }
 }
 
