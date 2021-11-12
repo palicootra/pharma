b69d81873106d8ec16eaf72a898b43f5010f8145
paul nyoumi <nyoumipaul@yahoo.fr>
2021-11-12 21:58:50 +0100

    trx

--- src/main/java/com/example/ProjetFInal/testGimac/GimacTransaction.java
+++ src/main/java/com/example/ProjetFInal/testGimac/GimacTransaction.java
@@ -8,6 +8,7 @@ import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.security.Timestamp;
+import java.util.List;
 
 @Document( "token")
 @Data
@@ -16,32 +17,36 @@ import java.security.Timestamp;
 public class GimacTransaction {
         @Id
         private String id;
-        private Long createtime;
-        private Long expirytime;
-        private String sendermobile;
-        private String receivermobile;
-        private String intent;
-        private String walletdestination;
-        @Indexed(unique=true,sparse=true)
-        private String vouchercode;
-        private String tomember;
-        private String frommember;
-        @Indexed(unique=true,sparse=true)
-        private String issuertrxref;
+        private Float  amount;
         @Indexed(unique=true,sparse=true)
         private String aquirertrxref;
         private String atm;
         private String bank;
-
-        private Float  amount;
+        private Long   createtime;
         private String currency;
         private String description;
-        private String state;
         private String error;
         private String error_description;
-        private Long validityduration;
-        private Long updatetime;
-
+        private Long   expirytime;
+        private String frommember;
+        private String intent;
+        @Indexed(unique=true,sparse=true)
+        private String issuertrxref;
+        private String receivermobile;
+        private String receivercustomerdata;
+        private String sendercustomerdata;
+        private String sendermobile;
+        private String state;
+        private String tomember;
+        private Long   updatetime;
+        private Long   validityduration;
+        @Indexed(unique=true,sparse=true)
+        private String vouchercode;
+        private String walletdestination;
+        private String walletsource;
+        private String direction;
+        private String action;
+        private List<String> states;
 
 
 }
