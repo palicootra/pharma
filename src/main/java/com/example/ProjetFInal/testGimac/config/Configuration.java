c528eea558f44a6e521f8aa55bc7baa78944cdbc
paul nyoumi <nyoumipaul@yahoo.fr>
2021-11-14 02:40:06 +0100

    thread

--- src/main/java/com/example/ProjetFInal/testGimac/config/Configuration.java
+++ src/main/java/com/example/ProjetFInal/testGimac/config/Configuration.java
@@ -2,12 +2,10 @@ package com.example.ProjetFInal.testGimac.config;
 
 public class Configuration {
 
-    public static String baseUrl= "https://dry-temple-95858.herokuapp.com/pp/" ;
     public static String authenticationLink="oauth/token";
     public static String transactionLink="payment/send";
     public static String updateLink="payment/update";
     public static String serverBaseUrl="http://154.72.148.105:8081/";
-    //public static String serverGimacLink="gimacpay/transaction/update";
 
     public static String Action_update_transfert="action=MiseAjour_Transaction_Gimac_TransfertMNO";
     public static String Action_incoming_transaction="action=Action_incoming_transaction";
