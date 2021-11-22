5680484e04ade52292304b4413f1c68d9c1bfda2
paul nyoumi <nyoumipaul@yahoo.fr>
2021-11-22 17:57:31 +0100

    update

--- src/main/java/com/example/ProjetFInal/testGimac/config/Configuration.java
+++ src/main/java/com/example/ProjetFInal/testGimac/config/Configuration.java
@@ -2,13 +2,11 @@ package com.example.ProjetFInal.testGimac.config;
 
 public class Configuration {
 
-    public static String authenticationLink="oauth/token";
-    public static String transactionLink="payment/send";
+
     public static String updateLink="gimacpay/payment/update";
     public static String serverBaseUrl="http://154.72.148.105:8081/";
 
-    public static String Action_update_transfert="action=MiseAjour_Transaction_Gimac_TransfertMNO";
-    public static String Action_incoming_transaction="action=Action_incoming_transaction";
+
 
 
 
