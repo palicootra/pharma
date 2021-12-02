c63d9f878a492cacd771751ce6e3df78d0e7955d
paul nyoumi <nyoumipaul@yahoo.fr>
2021-12-02 19:31:01 +0100

    tests gimac

--- src/main/java/com/example/ProjetFInal/testGimac/config/Configuration.java
+++ src/main/java/com/example/ProjetFInal/testGimac/config/Configuration.java
@@ -5,12 +5,5 @@ public class Configuration {
 
     public static String updateLink="gimacpay/payment/update";
     public static String serverBaseUrl="http://154.72.148.105:8081/";
-
-
-
-
-
-
-
-
+    
 }
