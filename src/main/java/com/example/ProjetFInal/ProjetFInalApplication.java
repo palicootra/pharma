5486fa516b118598cbdee189820ef871bd5e195c
paul nyoumi <nyoumipaul@yahoo.fr>
2022-02-10 18:39:15 +0100

    tx update

--- src/main/java/com/example/ProjetFInal/ProjetFInalApplication.java
+++ src/main/java/com/example/ProjetFInal/ProjetFInalApplication.java
@@ -3,8 +3,6 @@ package com.example.ProjetFInal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
@@ -15,10 +13,6 @@ public class ProjetFInalApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(ProjetFInalApplication.class, args);
-	}
-	@Bean
-	public PasswordEncoder passwordEncoder(){
-		return NoOpPasswordEncoder.getInstance();
 	}
 }
 
--- src/main/java/com/example/ProjetFInal/testGimac/GimacTransaction.java
+++ src/main/java/com/example/ProjetFInal/testGimac/GimacTransaction.java
@@ -1,26 +1,32 @@
 package com.example.ProjetFInal.testGimac;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.security.Timestamp;
 import java.util.List;
-import java.util.Random;
+import java.util.Map;
 
-@Document( "token")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Document( "Transaction")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class GimacTransaction {
+        @Id
+        private String id;
         private Float  amount;
         @Indexed(unique=true,sparse=true)
         private String acquirertrxref;
         private String atm;
         private String bank;
+        private Map<String,String> billInquiryData;
+        private List<Map<String,String>> billList;
+        private String contractRef;
+        private String billRef;
         private Long   createtime;
         private String currency;
         private String description;
@@ -32,11 +38,14 @@ public class GimacTransaction {
         @Indexed(unique=true,sparse=true)
         private String issuertrxref;
         private String receivermobile;
-        private String receivercustomerdata;
-        private String sendercustomerdata;
+        private Map<String,Object> receivercustomerdata;
+        private Map<String,Object> sendercustomerdata;
         private String sendermobile;
+        private String serviceRef;
+        private String service;
+
         private String state;
-        private String tomember;
+        private String toMember;
         private Long   updatetime;
         private Long   validityduration;
         @Indexed(unique=true,sparse=true)
@@ -45,13 +54,66 @@ public class GimacTransaction {
         private String walletsource;
         private String direction;
         private String action;
-        private List<String> states;
+        //differents states of the transaction in order
+        private List <String> states;
+
+        private GimacTransaction response;
 
 
-        public void generateVoucherCode() {
-                int rand=new Random().nextInt(100000);
-                int rand2=new Random().nextInt(1000000);
-                int rand3=new Random().nextInt(10000000);
-                this.setVouchercode("GIMAC"+ rand+rand2+rand3);
+
+        public GimacTransaction merge(GimacTransaction transaction) {
+                if(this.getAmount()==null ) this.setAmount(transaction.getAmount());
+                if(this.getIssuertrxref()==null ) this.setIssuertrxref(transaction.getIssuertrxref());
+                if(this.getAcquirertrxref()==null ) this.setAcquirertrxref(transaction.getAcquirertrxref());
+                if(this.getAtm()==null ) this.setAtm(transaction.getAtm());
+                if(this.getBank()==null ) this.setBank(transaction.getBank());
+                if(this.getBillInquiryData()==null) this.setBillInquiryData(transaction.getBillInquiryData());
+                if(this.getBillList()==null) this.setBillList(transaction.getBillList());
+                if(this.getBillRef()==null) this.setBillRef(transaction.getBillRef());
+                if(this.getContractRef()==null) this.setContractRef(transaction.getContractRef());
+                if(this.getCreatetime()==null ) this.setCreatetime(transaction.getCreatetime());
+                if(this.getCurrency()==null ) this.setCurrency(transaction.getCurrency());
+                if(this.getDescription()==null ) this.setDescription(transaction.getDescription());
+                if(this.getError()==null ) this.setError(transaction.getError());
+                if(this.getError_description()==null ) this.setError_description(transaction.getError_description());
+                if(this.getExpirytime()==null ) this.setExpirytime(transaction.getExpirytime());
+                if(this.getFrommember()==null ) this.setFrommember(transaction.getFrommember());
+                if(this.getIntent()==null ) this.setIntent(transaction.getIntent());
+                if(this.getIssuertrxref()==null ) this.setIssuertrxref(transaction.getIssuertrxref());
+                if(this.getReceivermobile()==null ) this.setReceivermobile(transaction.getReceivermobile());
+                if(this.getSendercustomerdata()==null ) this.setSendercustomerdata(transaction.getSendercustomerdata());
+                if(this.getReceivercustomerdata()==null ) this.setReceivercustomerdata(transaction.getReceivercustomerdata());
+
+
+                if(this.getSendermobile()==null ) this.setSendermobile(transaction.getSendermobile());
+                if(this.getService()==null ) this.setService(transaction.getService());
+
+                if(this.getServiceRef()==null ) this.setServiceRef(transaction.getServiceRef());
+                if(this.getState()==null ) this.setState(transaction.getState());
+                if(this.getToMember()==null ) this.setToMember(transaction.getToMember());
+                //always update updated time
+                if(transaction.getUpdatetime()!=null ) this.setUpdatetime(transaction.getUpdatetime());
+                if(this.getValidityduration()==null ) this.setValidityduration(transaction.getValidityduration());
+                if(this.getVouchercode()==null ) this.setVouchercode(transaction.getVouchercode());
+                if(this.getWalletdestination()==null ) this.setWalletdestination(transaction.getWalletdestination());
+                if(this.getWalletsource()==null) this.setWalletsource(transaction.getWalletsource());
+
+
+                return this;
+        }
+
+
+        public void cleanForGimac() {
+                this.setResponse(null);
+                this.setDirection(null);
+                this.setId(null);
+                this.setStates(null);
+                this.setAction(null);
+        }
+        public boolean verifyIncomingTransaction(){
+                if (this.toMember.equals(Constants.Strings.PERFECT_PAY)){
+                        return false;
+                }
+                return true;
         }
 }
